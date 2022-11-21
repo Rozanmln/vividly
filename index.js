92029fc1d21591bed6945b88f555462ee9574363
Rozan Maulana <rozanmln.wp@gmail.com>
2022-11-22 00:23:02 +0700

    post user

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
+const users = require('./routes/users');
 const home = require('./routes/home');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
@@ -27,6 +28,7 @@ app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
+app.use('/api/users', users);
 app.use('/', home);
 
 
