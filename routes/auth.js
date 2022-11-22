cbc00d68473e91d1f1ea71063485b26adc1608f6
Rozan Maulana <rozanmln.wp@gmail.com>
2022-11-22 11:35:31 +0700

    user authentication

---
+++ routes/auth.js
@@ -0,0 +1,40 @@
+const {User} = require('../models/user');
+const _ = require('lodash');
+const Joi = require('joi');
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    const users = await User.find().sort('name');
+    res.send(users);
+});
+
+router.post('/',  async (req,res) => {
+    const { error } = validate(req.body);
+
+    if (error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+    
+    let user = await User.findOne({email: req.body.email});
+    if (!user) return res.status(400).send('email atau password salah');
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if(!validPassword) return res.status(400).send('email atau password salah');
+
+    res.send(true);
+});
+
+function validate(req) {
+    const schema = {
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(5).max(255).required()
+    };
+
+    return Joi.validate(req, schema);
+}
+
+module.exports = router;
