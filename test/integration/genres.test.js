6825881b7cec5504a03a5e5a7397917e64d671c3
Rozan Maulana <rozanmln.wp@gmail.com>
2023-01-01 17:10:32 +0700

    stuck udh berhasil

--- test/integration/genres.test.js
+++ test/integration/genres.test.js
@@ -29,7 +29,7 @@ describe('/api/genres', () => {
             const genre = new Genre({name: 'action'});
             await genre.save();
 
-            const res = await request(server).get('/api/genres' + genre._id);
+            const res = await request(server).get('/api/genres/' + genre._id);
 
             expect(res.status).toBe(200);
             expect(res.body).toHaveProperty("name", genre.name);
